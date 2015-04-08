0aecdfb2115a945b43834c86c94a4454b7d635ab
Oleg Plakhotniuk <olegus8@gmail.com>
2015-04-08 14:58:10 -0500

    www

--- toothris-www/run/makewww.sh
+++ toothris-www/run/makewww.sh
@@ -3,6 +3,7 @@
 set -e
 
 TMPDIR=/var/tmp/toothris-www
+RESDIR=/toothris-www/toothris.git/src/res/game
 
 # main static content
 cp /toothris-www/www/* $TMPDIR
@@ -20,3 +21,10 @@ cp -r /toothris-www/jquery/node_modules/jquery/dist/cdn/* $TMPDIR
 # pics
 convert $TMPDIR/oleg.jpg -rotate 90 -resize 160x120 $TMPDIR/oleg-sm.jpg
 convert $TMPDIR/sshot.bmp $TMPDIR/sshot.jpg
+
+convert $RESDIR/128x128_bonus_super_drop.png \
+  -background "#00286D" -alpha remove \
+  -resize 64x64 $TMPDIR/bonus-super-drop.jpg
+convert $RESDIR/128x128_event_drop.png \
+  -background "#00286D" -alpha remove \
+  -resize 64x64 $TMPDIR/action-drop.jpg
